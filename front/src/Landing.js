ab46cd97ef623aca867fa5afaa1484ef5c12d736
barab <94920370+Baraban511@users.noreply.github.com>
2024-04-12 11:31:45 +0200

    Ajout page 404

--- front/src/Landing.js
+++ front/src/Landing.js
@@ -33,10 +33,10 @@ export default function Landing() {
                 Get started
               </a>
               <a
-                href="/data"
+                href="/add"
                 className="text-sm font-semibold leading-6 text-gray-900"
               >
-                Learn more <span aria-hidden="true">→</span>
+                Add data <span aria-hidden="true">→</span>
               </a>
             </div>
           </div>
--- front/src/index.js
+++ front/src/index.js
@@ -15,6 +15,7 @@ const router = createBrowserRouter([
   {
     path: "/",
     element: <Landing />,
+    errorElement: <Landing />,
   },
   {
     path: "/map",
