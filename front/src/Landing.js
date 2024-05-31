42a1b6a2ca4009203881d0098159ec840dbcba34
barab <94920370+Baraban511@users.noreply.github.com>
2024-05-31 14:17:48 +0200

    Fix: Tailwind cdn

--- front/src/Landing.js
+++ front/src/Landing.js
@@ -21,9 +21,7 @@ export default function Landing() {
               Data to enrich your online business
             </h1>
             <p className="mt-6 text-lg leading-8 text-gray-600">
-              Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui
-              lorem cupidatat commodo. Elit sunt amet fugiat veniam occaecat
-              fugiat aliqua.
+              Made by <strong><a href="https://barab.live">barab</a></strong>
             </p>
             <div className="mt-10 flex items-center justify-center gap-x-6">
               <a
